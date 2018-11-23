0a0c9b2d3928a08d7b7d24b7d7474a9257d3bb5d
unknown <nroutier@gmail.com>
2018-11-23 18:36:31 +0100

    test Pygame

--- MacGyver/constant.py
+++ MacGyver/constant.py
@@ -1,2 +1,4 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+print(1+1)
--- MacGyver/macgyvergame.py
+++ MacGyver/macgyvergame.py
@@ -13,15 +13,19 @@ with open("level.txt", "r") as level:
                 line.append(char)
         bg.append(line)
 
-# print(bg)
+print(bg)
 
 ind_e = []
 
+ind_y = 0
 for y in bg:
+    ind_x = 0
     for x in y:
         if x == 'e':
-            ind_e.append((bg.index(y), y.index(x)))
-
+            ind_e.append((ind_y, ind_x))
+        ind_x += 1
+    ind_y += 1
+        
 # r = random.choice(ind_e)
 # print(r)
 
@@ -29,6 +33,8 @@ class Objs:
     def __init__(self, name):
         self.name = name
         self.position = ()
+
+l2 = list(ind_e)
 
 O1 = Objs("O1")
 O2 = Objs("O2")
@@ -38,11 +44,52 @@ G = Objs("G")
 OO = [O1, O2, O3, G]
 
 for O in OO:
-    r = random.choice(ind_e)
+    r = random.choice(l2)
     O.position = r
-    ind_e.remove(r)
+    l2.remove(r)
 
 print(O1.position)
 print(O2.position)
 print(O3.position)
 print(G.position)
+
+l2 == ind_e
+print(l2)
+print(ind_e)
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+#Ouverture de la fenêtre Pygame
+fenetre = pygame.display.set_mode((640, 480))
+
+#Chargement et collage du fond
+fond = pygame.image.load("background.jpg").convert()
+fenetre.blit(fond, (0,0))
+
+#Chargement et collage du personnage
+perso = pygame.image.load("perso.png").convert_alpha()
+position_perso = perso.get_rect()
+fenetre.blit(perso, position_perso)
+
+#Rafraîchissement de l'écran
+pygame.display.flip()
+
+#BOUCLE INFINIE
+continuer = 1
+while continuer:
+	for event in pygame.event.get():	#Attente des événements
+		if event.type == QUIT:
+			continuer = 0
+		if event.type == KEYDOWN:
+			if event.key == K_DOWN:	#Si "flèche bas"
+				#On descend le perso
+				position_perso = position_perso.move(0,3)
+	
+	#Re-collage
+	fenetre.blit(fond, (0,0))	
+	fenetre.blit(perso, position_perso)
+	#Rafraichissement
+	pygame.display.flip()
